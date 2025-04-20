adca63ed70fcd96d38c1f381ec8637c957ca1caf
hyeon924 <hyeonn4713@gmail.com>
2025-04-20 21:38:08 +0900

    입력상태 콘솔 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [region, setRegion] = useState('서울');
+  const [category, setCategory] = useState('한식');
+
+  const handleSearch = () => {
+    console.log('선택된 지역', region);
+    console.log('선택된 종류', category);
+  }
+
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
       <div className="flex flex-col items-center gap-6">
@@ -10,21 +19,21 @@ function App() {
         </h1>
 
         <div className="flex gap-4">
-          <select className="border px-4 py-2 rounded">
+          <select className="border px-4 py-2 rounded" value={region} onChange={(e) => setRegion(e.target.value)}>
             <option>서울</option>
             <option>부산</option>
             <option>대전</option>
             <option>대구</option>
           </select>
 
-          <select className="border px-4 py-2 rounded">
+          <select className="border px-4 py-2 rounded" value={category} onChange={(e) => setCategory(e.target.value)}>
             <option>한식</option>
             <option>중식</option>
             <option>일식</option>
             <option>양식</option>
           </select>
 
-          <button className="bg-blue-500 text-black px-4 py-2 rounded hover:bg-blue-600">
+          <button onClick={handleSearch} className="bg-blue-500 text-black px-4 py-2 rounded hover:bg-blue-600">
             검색
           </button>
         </div>
